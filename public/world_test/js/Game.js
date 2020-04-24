814f57b3efb6130c3156b336c30b6df62dcf1456
Billy Wong <billy.wong@stonybrook.edu>
2020-04-24 18:16:14 -0400

    added level format example to what we have

--- public/world_test/js/Game.js
+++ public/world_test/js/Game.js
@@ -6,6 +6,10 @@ import { Sheep } from "./Sheep.js";
 class Game {
     constructor() {}
 
+    setGame(game) {
+        this.game = game;
+    }
+
     setPlayer(player) {
         this.player = player;
     }
@@ -16,6 +20,122 @@ class Game {
 
     setSheepGroup(sheepGroup) {
         this.sheepGroup = sheepGroup;
+    }
+}
+
+//currently messy since outside level classes
+var PlayerControls;
+var DogPlayer;
+var sheepAI;
+
+export var dog;
+export var fences;
+export var player;
+export var sheep;
+
+class TestLevel extends Phaser.Scene {
+    constructor() {
+        super({key: 'TestLevel'});
+    }
+
+
+    preload () {
+        this.load.image('dog', 'assets/058.png');
+        this.load.image('sheep', 'assets/sheep.png');
+    }
+    
+    create () {
+        dog = this.physics.add.group({
+            defaultKey: "dog"
+        });
+        player = dog.create(960, 540);
+        player.body.collideWorldBounds = true;
+    
+        sheep = this.physics.add.group({
+            defaultKey: "sheep"
+        });
+    
+        fences = this.physics.add.staticGroup();
+        this.physics.add.collider(player, fences);
+    
+        //console.log(player);
+    
+        var cursors = this.input.keyboard.createCursorKeys();
+        var spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        var pointer = this.input.activePointer;
+
+        this.cKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
+
+        PlayerControls = new Controls(this, cursors, player, spaceKey, pointer, sheep);
+        DogPlayer = new Dog(game, player, sheep, fences);
+        sheepAI = new Sheep(game, DogPlayer, "IDLE", sheep.create(961, 541));
+    
+        //Can I move this to controls?
+        this.input.on("pointerup", function(pointer) {
+            console.log("lasso");
+        });
+    }   
+    
+    update() {
+        PlayerControls.check();
+        sheepAI.update();
+        if (Phaser.Input.Keyboard.JustDown(this.cKey)) {
+            console.log("cee1");
+            this.scene.start('TestLevel2');
+        }
+    }
+}
+
+class TestLevel2 extends Phaser.Scene {
+    constructor() {
+        super({key: 'TestLevel2'});
+    }
+
+
+    preload () {
+        this.load.image('dog', 'assets/058.png');
+        this.load.image('sheep', 'assets/sheep.png');
+    }
+    
+    create () {
+        dog = this.physics.add.group({
+            defaultKey: "dog"
+        });
+        player = dog.create(50, 50);
+        player.body.collideWorldBounds = true;
+    
+        sheep = this.physics.add.group({
+            defaultKey: "sheep"
+        });
+    
+        fences = this.physics.add.staticGroup();
+        this.physics.add.collider(player, fences);
+    
+        //console.log(player);
+    
+        var cursors = this.input.keyboard.createCursorKeys();
+        var spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        var pointer = this.input.activePointer;
+
+        this.cKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
+
+        PlayerControls = new Controls(this, cursors, player, spaceKey, pointer, sheep);
+        DogPlayer = new Dog(game, player, sheep, fences);
+        sheepAI = new Sheep(game, DogPlayer, "IDLE", sheep.create(961, 541));
+    
+        //Can I move this to controls?
+        this.input.on("pointerup", function(pointer) {
+            console.log("lasso");
+        });
+    }   
+    
+    update() {
+        PlayerControls.check();
+        sheepAI.update();
+        if (Phaser.Input.Keyboard.JustDown(this.cKey)) {
+            console.log("cee2");
+            this.scene.start('TestLevel');
+        }
     }
 }
 
@@ -31,22 +151,11 @@ var config = {
             gravity: { y: 0 }
         }
     },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    scene: [ TestLevel, TestLevel2 ]
 };
 
 var game = new Phaser.Game(config);
-var PlayerControls;
-var DogPlayer;
-var sheepAI;
 
-export var dog;
-export var fences;
-export var player;
-export var sheep;
 
 function preload () {
     this.load.image('dog', 'assets/058.png');
@@ -87,26 +196,10 @@ function update() {
     sheepAI.update();
 }
 
-//get sheep positions
-//get dog positions
-//get pond positions
-//get fence positions
 
-//fence/pond?
 
-/*
-Dog = game.physics.add.group({
-    defaultKey: "dog"
-});
-player = Dog.create(960, 540);
-player.body.collideWorldBounds = true;
-
-Fences = game.physics.add.staticGroup();
-game.physics.add.collider(player, Fences);
-
-console.log(player);
-
-cursors = game.input.keyboard.createCursorKeys();
-controls = new Controls(game, cursors, player);
-*/
-
+var GameObject = new Game();
+GameObject.setGame(game);
+GameObject.setPlayer(DogPlayer);
+GameObject.setFencesGroup(fences);
+GameObject.setSheepGroup(sheep);
