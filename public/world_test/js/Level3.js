8ab0722645e6e2dc2e074c5a061efd18d31f724a
snisonoff <spencer.nisonoff@stonybrook.edu>
2020-04-26 17:55:13 -0400

    Implemented Level 3

--- public/world_test/js/Level3.js
+++ public/world_test/js/Level3.js
@@ -9,11 +9,52 @@ export class Level3 extends Level {
         super.create();
     
         this.setRequiredScore(500);
-        this.setPlayerPosition(800, 800);
-        this.createSheep(836, 800);
+        this.setPlayerPosition(368, 1000);
+        this.createSheep(332, 968);
+        this.createSheep(400, 968);
 
-        this.createPond(70,600);
-        this.createPond(70,632);
+        //Left side
+        for(var i = 0; i < 9; i++) {
+            this.createPond(68 + (32 * i),216);
+            this.createPond(68 + (32 * i),248);
+            this.createPond(68 + (32 * i),280);
+            this.createPond(68 + (32 * i),312);
+            this.createPond(68 + (32 * i),344);
+            this.createPond(68 + (32 * i),376);
+            this.createPond(68 + (32 * i),408);
+            this.createPond(68 + (32 * i),440);
+            this.createPond(68 + (32 * i),472);
+            this.createPond(68 + (32 * i),504);
+            this.createPond(68 + (32 * i),536);
+            this.createPond(68 + (32 * i),568);
+            this.createPond(68 + (32 * i),600);
+            this.createPond(68 + (32 * i),632);
+            this.createPond(68 + (32 * i),664);
+            this.createPond(68 + (32 * i),696);
+            this.createPond(68 + (32 * i),728);
+        }
+
+        //right side
+        for(var i = 0; i < 9; i++) {
+            this.createPond(400 + (32 * i),216);
+            this.createPond(400 + (32 * i),248);
+            this.createPond(400 + (32 * i),280);
+            this.createPond(400 + (32 * i),312);
+            this.createPond(400 + (32 * i),344);
+            this.createPond(400 + (32 * i),376);
+            this.createPond(400 + (32 * i),408);
+            this.createPond(400 + (32 * i),440);
+            this.createPond(400 + (32 * i),472);
+            this.createPond(400 + (32 * i),504);
+            this.createPond(400 + (32 * i),536);
+            this.createPond(400 + (32 * i),568);
+            this.createPond(400 + (32 * i),600);
+            this.createPond(400 + (32 * i),632);
+            this.createPond(400 + (32 * i),664);
+            this.createPond(400 + (32 * i),696);
+            this.createPond(400 + (32 * i),728);
+        }
+
 
         this.createBoxOfFences(32, 32, 20, 30);
         this.createFinishSpace(32, 32, 672, 150);
