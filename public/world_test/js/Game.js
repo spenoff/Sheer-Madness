19efb4ed547105622334d1a84eb721cf3eeaf04d
ndeng0 <47835297+ndeng0@users.noreply.github.com>
2020-04-27 21:28:48 -0400

    Update Game.js

--- public/world_test/js/Game.js
+++ public/world_test/js/Game.js
@@ -19,6 +19,7 @@ class MainMenu extends GameScene {
         
         this.load.image('mainbackground', 'assets/Background.png');
         this.load.spritesheet('buttons', 'assets/Buttons.png', {frameWidth: 506, frameHeight: 105});
+        this.load.spritesheet('titles', 'assets/Titles.png', {frameWidth: 801, frameHeight: 200});
 
     }
 
@@ -28,17 +29,19 @@ class MainMenu extends GameScene {
         var background = this.add.tileSprite(960, 540, 0, 0, 'mainbackground');
         background.setDepth(-1);
 
-        var start       = this.add.sprite(960, 360, 'buttons', 0).setInteractive();
-        var levelsel    = this.add.sprite(960, 480, 'buttons', 1).setInteractive();
-        var setting     = this.add.sprite(960, 600, 'buttons', 2).setInteractive();
-        var about       = this.add.sprite(960, 720, 'buttons', 3).setInteractive();
+        var mmtitle     = this.add.sprite(960, 200, 'titles', 0);
+
+        var start       = this.add.sprite(960, 450, 'buttons', 0).setInteractive();
+        var levelsel    = this.add.sprite(960, 570, 'buttons', 1).setInteractive();
+        var setting     = this.add.sprite(960, 690, 'buttons', 2).setInteractive();
+        var about       = this.add.sprite(960, 810, 'buttons', 3).setInteractive();
 
         start.on('pointerdown', function(event) {
             this.scene.start('Level0');
         }, this);
-        levelsel.on('pointerdown', function() {
-            alert("levelsel");
-        });
+        levelsel.on('pointerdown', function(event) {
+            this.scene.start('LevelSelectMenu')
+        }, this);
         setting.on('pointerdown', function(event) {
             this.scene.start('SettingMenu');
         }, this);
@@ -53,6 +56,63 @@ class MainMenu extends GameScene {
 
 }
 
+class LevelSelectMenu extends GameScene {
+
+    constructor() {
+        super('LevelSelectMenu');
+    }
+
+    preload() {
+        
+        this.load.image('mainbackground', 'assets/Background.png');
+        this.load.spritesheet('titles', 'Titles.png', {frameWidth: 801, frameHeight: 200});
+        this.load.spritesheet('levelselectbuttons', 'assets/Levels.png', {frameWidth: 105, frameHeight: 105});
+
+    }
+
+    create() {
+        super.create();
+
+        var background = this.add.tileSprite(960, 540, 0, 0, 'mainbackground');
+        background.setDepth(-1);
+
+        var lstitle = this.add.sprite(960, 200, 'titles', 1);
+
+        var l1      = this.add.sprite(585, 450, 'levelselectbuttons', 0).setInteractive();
+        var l2      = this.add.sprite(735, 450, 'levelselectbuttons', 1).setInteractive();
+        var l3      = this.add.sprite(885, 450, 'levelselectbuttons', 2).setInteractive();
+        var l4      = this.add.sprite(1035, 450, 'levelselectbuttons', 3).setInteractive();
+        var l5      = this.add.sprite(1185, 450, 'levelselectbuttons', 4).setInteractive();
+        var l6      = this.add.sprite(1335, 450, 'levelselectbuttons', 5).setInteractive();
+        var l7      = this.add.sprite(585, 600, 'levelselectbuttons', 6).setInteractive();
+        var l8      = this.add.sprite(735, 600, 'levelselectbuttons', 7).setInteractive();
+        var l9      = this.add.sprite(885, 600, 'levelselectbuttons', 8).setInteractive();
+        var l10     = this.add.sprite(1035, 600, 'levelselectbuttons', 9).setInteractive();
+        var l11     = this.add.sprite(1185, 600, 'levelselectbuttons', 10).setInteractive();
+        var lock    = this.add.sprite(1335, 600, 'levelselectbuttons', 11).setInteractive();
+
+        l1.on('pointerdown', function(event) {
+            this.scene.start('Level1');
+        }, this);
+        l2.on('pointerdown', function(event) {
+            this.scene.start('Level2');
+        }, this);
+        l3.on('pointerdown', function(event) {
+            this.scene.start('Level3');
+        }, this);
+        l4.on('pointerdown', function(event) {
+            this.scene.start('Level4');
+        }, this);
+        lock.on('pointerdown', function(event) {
+            this.scene.start('Level0');
+        }, this);
+    }
+
+    update() {
+        super.update();
+    }
+}
+
 class SettingMenu extends GameScene {
 
     constructor() {
@@ -60,7 +120,9 @@ class SettingMenu extends GameScene {
     }
 
     preload() {
+
         this.load.image("settingmenu", 'assets/Controls.png');
+
     }
 
     create() {
@@ -112,7 +174,7 @@ var config = {
             gravity: { y: 0 }
         }
     },
-    scene : [ MainMenu, AboutMenu, SettingMenu, Level0, Level1, Level2, Level3, Level4 ]
+    scene : [ MainMenu, LevelSelectMenu, AboutMenu, SettingMenu, Level0, Level1, Level2, Level3, Level4 ]
 };
 
 var game = new Phaser.Game(config);
