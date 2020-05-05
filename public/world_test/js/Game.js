7470a43e7908b1ef02977ef4269e5926bc81032c
ndeng0 <47835297+ndeng0@users.noreply.github.com>
2020-05-05 15:57:34 -0400

    Screen changes

--- public/world_test/js/Game.js
+++ public/world_test/js/Game.js
@@ -241,6 +241,10 @@ var config = {
     type: Phaser.AUTO,
     width: 1920,
     height: 1080,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     physics: {
         default: 'arcade',
         arcade: {
