e5f25a831b00e34156ee5276c708baf7c911edbf
Billy Wong <billy.wong@stonybrook.edu>
2020-05-22 12:26:08 -0400

    sheep diagonal body size fix

--- public/js/Level.js
+++ public/js/Level.js
@@ -311,6 +311,12 @@ export class Level extends GameScene {
                 if (sheep.asset.body.velocity.x != 0 || sheep.asset.body.velocity.y != 0) {
                     sheep.asset.angle = 90 + Math.atan2(sheep.asset.body.velocity.y, sheep.asset.body.velocity.x) * 180 / Math.PI;
                 }
+                if (sheep.asset.angle % 90 == 0) {
+                    sheep.asset.body.setSize(11, 11);
+                }
+                else {
+                    sheep.asset.body.setSize();
+                }
             });
 
             if(this.player.lassoTarget != null && this.player.lassoAsset == null) {
