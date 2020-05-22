5511f4e0b9ac03a0d7c62474fb5837d4fe55acfc
Billy Wong <billy.wong@stonybrook.edu>
2020-05-22 12:44:40 -0400

    split angles

--- public/world_test/js/Level.js
+++ public/world_test/js/Level.js
@@ -311,12 +311,18 @@ export class Level extends GameScene {
                 if (sheep.asset.body.velocity.x != 0 || sheep.asset.body.velocity.y != 0) {
                     sheep.asset.angle = 90 + Math.atan2(sheep.asset.body.velocity.y, sheep.asset.body.velocity.x) * 180 / Math.PI;
                 }
-                if (sheep.asset.angle % 90 == 0) {
+                if (sheep.asset.angle % 180 == 0) {
                     sheep.asset.body.setSize(11, 11);
                 }
+                
+                else if (sheep.asset.angle % 90 == 0) {
+                    sheep.asset.body.setSize(11, 11);
+                }
+                
                 else {
                     sheep.asset.body.setSize();
                 }
+                //console.log(sheep.asset.angle, sheep.asset.angle % 90);
             });
 
             if(this.player.lassoTarget != null && this.player.lassoAsset == null) {
