ca25615587dfd28b749b10be5395a987b10516c5
Billy Wong <billy.wong@stonybrook.edu>
2020-05-25 19:22:27 -0400

    added sheep can't get too close to fences, sheep can no longer be pushed

--- public/world_test/js/Level0.js
+++ public/world_test/js/Level0.js
@@ -9,16 +9,18 @@ export class Level0 extends Level {
     create() {
         super.create();
     
-        this.setPlayerPosition(800, 800);
+        this.setPlayerPosition(900, 800);
         this.createSheep(900, 600);
         this.createPond(400, 700);
         this.createFinishSpace(800, 100, 128, 128);
 
-        this.createVerticalFences(400, 32, 10);
-        this.createHorizontalFences(600, 700, 15);
+        //this.createVerticalFences(400, 32, 10);
+        //this.createHorizontalFences(600, 700, 15);
 
-        this.createWolf(200, 400, 120, 120, 1000, 1500);
-        this.createWolf(300, 700, 120, 0, 1500)
+        //this.createWolf(200, 400, 120, 120, 1000, 1500);
+        //this.createWolf(300, 700, 120, 0, 1500)
+        //this.createBoxOfFences(0, 0, 60, 30);
+        this.createHorizontalFences(900, 450, 1, 1, true, true, true);
     }
 
     update() {
